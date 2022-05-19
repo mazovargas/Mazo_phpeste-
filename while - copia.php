594f35de9d9e92dd6df5fdb8c79b700483d4f0ab
element <juan.mazo40@misena.edu.co>
2022-05-18 20:18:45 -0500

    commit ramamazo

---
+++ while - copia.php
@@ -0,0 +1,18 @@
+<?php
+//++: incremento en 1
+//+ 1: incremento de 1 en 1
+//+ 2: incremento de 2 en 2
+//Acumulador: cambia de valor con una variable
+//Contador: cambia de valor con una constante
+/*
+ciclo while: mientras que.
+se ejecuta mientras se cumpla la condicion o condiciones
+*/
+$contador = 1;
+while($contador <=10){
+    echo $contador."<br>";
+   $contador = $contador+ 1; //incremento de 1 en 1
+}
+
+
+?>
